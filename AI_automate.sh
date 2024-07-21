36e2dad4e828512bc8f5782f9f75869efd8f8730
Yoonchulchung <yoonchul005@gmail.com>
2024-07-21 21:54:54 +0900

    updated

--- AI_automate.sh
+++ AI_automate.sh
@@ -9,7 +9,10 @@ figlet AEYE AI
 # install dependencies
 sudo pip install -r dependencies_AI.txt
 
-# install docker
+####################
+## install docker ##
+####################
+figlet Install Docker
 sudo apt install -y \
     ca-certificates \
     curl \
@@ -28,7 +31,11 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 # Create the docker group.
 sudo groupadd docker
 
-# install Nvidia Docker
+###########################
+## install Nvidia Docker ##
+###########################
+figlet Install 
+figlet Nvidia Docker
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
@@ -39,6 +46,11 @@ sudo apt install -y nvidia-docker2
 sudo systemctl restart docker
 
 sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
+
+###########
+## READY ##
+###########
+figlet Ready!
 
 
 # run server
