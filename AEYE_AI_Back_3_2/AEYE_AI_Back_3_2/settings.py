ab19724103656a750065d7d027f12b7b79d8bae7
Yoonchulchung <yoonchul005@gmail.com>
2024-07-28 16:50:06 +0900

    updated

--- AEYE_AI_Back_3_2/AEYE_AI_Back_3_2/settings.py
+++ AEYE_AI_Back_3_2/AEYE_AI_Back_3_2/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom APP
+    'data',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
